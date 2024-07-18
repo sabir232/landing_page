6a11efcc542a223a3c4f5dc9d5f5ec277c42208c
sabir232 <sabirkhan98563@gmail.com>
2024-07-18 16:26:27 +0530

    implemented component and added new svg with animation for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import BeBestYou from "@/components/BeBestYou";
 import DoesThisSoundFamiliar from "@/components/DoesThisSoundFamiliar";
 import EQbeatsIQ from "@/components/EQbeatsIQ";
+import EverWonder from "@/components/EverWonder";
 import Footer from "@/components/Footer";
 import MasterLife from "@/components/MasterLife";
 import MeetAheadCard from "@/components/MeetAheadCard";
 import Navbar from "@/components/Navbar";
 import SelfImprovement from "@/components/SelfImprovement";
+import Test from "@/components/Test";
 import Image from "next/image";
 
 export default function Home() {
@@ -16,6 +19,9 @@ export default function Home() {
       <DoesThisSoundFamiliar />
       <MeetAheadCard />
       <SelfImprovement />
+      <BeBestYou />
+      <EverWonder />
+      <Test />
       <Footer />
     </>
   );
